65998d9fc0b2c8603060042cc538f91628a18337
i66a <51226544+i66a@users.noreply.github.com>
2024-08-17 15:24:46 +0800

    usetotest20240817
    usetotest20240817

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Dooy/chatgpt-web-midjourney-proxy/main/src/assets/avatar.jpg',
+      avatar: 'https://www.i66a.com/avatar.jpg',
       name:  t('mjset.sysname'),//'AI绘图',
-      description: 'Star on <a href="https://github.com/Dooy/chatgpt-web-midjourney-proxy" class="text-blue-500" target="_blank" >GitHub</a>',
+      description: 'Star on <a href="javascript:void(0);" class="text-blue-500" target="_blank" >LINJUNJIE</a>',
     },
   }
 }
